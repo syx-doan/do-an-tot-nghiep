1566bac5c2d188c549c5684a2714529a987b3330
Nguyen Thai Quang Ha <quangha2001cute@gmail.com>
2022-11-19 11:53:33 +0700

    san pham + category

--- src/components/shops/Catg.jsx
+++ src/components/shops/Catg.jsx
@@ -1,49 +1,36 @@
-import React from "react"
-
+import React, { useEffect, useState } from "react"
+import axiosClient from './../../utils/http';
 const Catg = () => {
-  const data = [
-    {
-      cateImg: "./images/category/cat-1.png",
-      cateName: "Apple",
-    },
-    {
-      cateImg: "./images/category/cat-2.png",
-      cateName: "Samasung",
-    },
-    {
-      cateImg: "./images/category/cat-1.png",
-      cateName: "Oppo",
-    },
-    {
-      cateImg: "./images/category/cat-2.png",
-      cateName: "Vivo",
-    },
-    {
-      cateImg: "./images/category/cat-1.png",
-      cateName: "Redimi",
-    },
-    {
-      cateImg: "./images/category/cat-2.png",
-      cateName: "Sony",
-    },
-  ]
+  const [data, setData] = useState([]);
+  const fetchPost = async () => {
+    try {
+        const response = await axiosClient('category');
+        setData(response.data);
+        console.log(response);
+    } catch (err) {
+        console.error(err);
+    }
+};
+useEffect(() => {
+    fetchPost();
+}, []);
   return (
     <>
       <div className='category'>
         <div className='chead d_flex'>
-          <h1>Brands </h1>
-          <h1>Shops </h1>
+          <h1>Category</h1>
+          <h1>Shops</h1>
         </div>
         {data.map((value, index) =>  (
             <div className='box f_flex' key={index}>
-              <img src={value.cateImg} alt='' />
-              <span>{value.cateName}</span>
+              {/* <img src={`http://192.168.43.50/duan/admin_dasboard/upload/thuonghieu/${value.image_brand}`} alt='' /> */}
+              <span>{value.name_category}</span>
             </div>
           )
         )}
-        <div className='box box2'>
+        {/* <div className='box box2'>
           <button>View All Brands</button>
-        </div>
+        </div> */}
       </div>
     </>
   )
