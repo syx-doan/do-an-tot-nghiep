47c85708da280200ef184bb16d5c1dd74f14a75d
syx-doan <85473426+syx-doan@users.noreply.github.com>
2022-11-26 20:05:15 +0700

    fix footer

--- src/common/footer/Footer.jsx
+++ src/common/footer/Footer.jsx
@@ -1,56 +1,60 @@
-import React from "react"
-import "./footer.css"
+import React from 'react';
+import './footer.css';
 
 const Footer = () => {
-  return (
-    <>
-      <footer>
-        <div className='container grid2'>
-          <div className='box'>
-            <h1>CLICKME</h1>
-            <p>Chúng tôi tin rằng chúng tôi là xuất sắc. Không phải vì chúng tôi nói nó, mà bởi vì chúng tôi làm việc chăm chỉ. Chúng tôi tận tâm, tận tụy và tập trung.</p>
-            <div className='icon d_flex'>
-              <div className='img d_flex'>
-                <i class='fa-brands fa-google-play'></i>
-                <span>Google Play</span>
-              </div>
-              <div className='img d_flex'>
-                <i class='fa-brands fa-app-store-ios'></i>
-                <span>App Store</span>
-              </div>
-            </div>
-          </div>
+    return (
+        <>
+            <footer>
+                <div className="container grid2">
+                    <div className="box">
+                        <h1>CLICKME</h1>
+                        <p>
+                            Chúng tôi tin rằng chúng tôi là xuất sắc. Không phải vì chúng tôi nói
+                            nó, mà bởi vì chúng tôi làm việc chăm chỉ. Chúng tôi tận tâm, tận tụy và
+                            tập trung.
+                        </p>
+                        <div className="icon d_flex">
+                            <div className="img d_flex">
+                                <i class="fa-brands fa-google-play"></i>
+                                <span>Google Play</span>
+                            </div>
+                            <div className="img d_flex">
+                                <i class="fa-brands fa-app-store-ios"></i>
+                                <span>App Store</span>
+                            </div>
+                        </div>
+                    </div>
 
-          <div className='box-footer'>
-            <h3>Sản phẩm</h3>
-            <ul className="paddingleft">
-              <li>Điện thoại</li>
-              <li>Máy tính</li>
-              <li>Laptop</li>
-              <li>Tai nghe</li>
-            </ul>
-          </div>
-          <div className='box-footer'>
-            <h3>Chăm sóc khách hàng</h3>
-            <ul>
-              <li>Mua thế nào </li>
-              <li>Theo dõi đơn hàng </li>
-              <li>Công ty & Mua hàng loạt </li>
-              <li>Trả lại và hoàn lại tiền</li>
-            </ul>
-          </div>
-          <div className='box-footer'>
-            <h3>Liên hệ</h3>
-            <ul>
-              <li>137 Nguyễn Thị Thập, Liên Chiểu, Đà Nẵng </li>
-              <li>Email: abc.help@gmail.com</li>
-              <li>Phone: +0 123 456 780</li>
-            </ul>
-          </div>
-        </div>
-      </footer>
-    </>
-  )
-}
+                    <div className="box-footer">
+                        <h3>Sản phẩm</h3>
+                        <ul className="paddingleft">
+                            <li>Điện thoại</li>
+                            <li>Máy tính</li>
+                            <li>Laptop</li>
+                            <li>Tai nghe</li>
+                        </ul>
+                    </div>
+                    <div className="box-footer">
+                        <h3>Chăm sóc khách hàng</h3>
+                        <ul>
+                            <li>Mua thế nào </li>
+                            <li>Theo dõi đơn hàng </li>
+                            <li>Công ty & Mua hàng loạt </li>
+                            <li>Trả lại và hoàn lại tiền</li>
+                        </ul>
+                    </div>
+                    <div className="box-footer">
+                        <h3>Liên hệ</h3>
+                        <ul>
+                            <li>137 Nguyễn Thị Thập, Liên Chiểu, Đà Nẵng </li>
+                            <li>Email: abc.help@gmail.com</li>
+                            <li>Phone: +0 123 456 780</li>
+                        </ul>
+                    </div>
+                </div>
+            </footer>
+        </>
+    );
+};
 
-export default Footer
+export default Footer;
